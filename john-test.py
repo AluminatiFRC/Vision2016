ee43b279185189f25be582e27c992f07db2e2f6b
John Rush <john@blakelystreet.com>
2016-01-27 21:31:01 -0800

    Only increment the fps counter when we know we have a good frame.

--- john-test.py
+++ john-test.py
@@ -4,6 +4,10 @@ import numpy as np
 import cv2
 
 camera = cv2.VideoCapture(0)
+#camera.set(cv2.cv.CV_CAP_PROP_FPS, 15)
+#camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640)
+#camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
+
 fpsDisplay = False;
 fpsCounter = WeightedFramerateCounter()
 fpsInterval = RealtimeInterval(3.0)
@@ -12,8 +16,8 @@ while (True):
     ret, raw = camera.read()
     if ret:
         cv2.imshow("raw", raw)
-
-    fpsCounter.tick()
+        fpsCounter.tick()
+    
     if fpsDisplay and fpsInterval.hasElapsed():
         print "{0:.1f} fps".format(fpsCounter.getFramerate())
     
