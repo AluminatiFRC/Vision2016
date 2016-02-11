68e81dad41657355ddb12c6d3bc505b904a5e82b
John Rush <john@blakelystreet.com>
2016-02-10 21:51:17 -0800

    A big cleanup. MQTT messages now indicate if there is a target or not. Easy to toggle between debug/release mode. Easier to calibrate camera focal length.

--- vision.py
+++ vision.py
@@ -3,7 +3,7 @@ import paho.mqtt.client as mqtt
 from WeightedFramerateCounter import WeightedFramerateCounter
 from RealtimeInterval import RealtimeInterval
 from CVParameterGroup import CVParameterGroup
-from TriangleSimilarityDistanceCalculator import TriangleSimilarityDistanceCalculator 
+import TriangleSimilarityDistanceCalculator as DistanceCalculator
 import numpy as np
 import cv2
 import time
@@ -11,36 +11,33 @@ import mqttClient
 import json
 
 debugMode = True
-tuneFOV = False
 
-targetSize = (11.25, 44) # size, distance (in inches)
+tuneDistance = False and debugMode
+
+BLUECASE_WIDTH = 14
+BLUECASE_HEIGHT = 10.5
+TESTTAPE_WIDTH = 11.25
+TESTTAPE_HEIGHT = 2
+TARGET_WIDTH = TESTTAPE_WIDTH
+TARGET_HEIGHT = TESTTAPE_HEIGHT
+TARGET_CALIBRATION_DISTANCE = 67
 
 def filterHue(source, hue, hueWidth, low, high):
     MAX_HUE = 179
     hsv = cv2.cvtColor(source, cv2.COLOR_BGR2HSV)
-    x = hue - hueWidth
-    if x < 0:
-         x = MAX_HUE - (MAX_HUE % x)
-    lowFilter = np.array([x, low, low])
 
-    x = hue + hueWidth
-    if x > MAX_HUE:
-         x = MAX_HUE % x
-    highFilter = np.array([x, high, low])
+    lowHue = max(hue - hueWidth, 0)
+    lowFilter = np.array([lowHue, low, low])
 
-    mask = cv2.inRange(hsv, lowFilter, highFilter)
-    return mask
-
-def findLargestContour(source):
-    contours, hierarchy = cv2.findContours(source, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    if len(contours) > 0:
-        ordered = sorted(contours, key = cv2.contourArea, reverse = True)[:1]
-        return ordered[0]
+    highHue = min(hue + hueWidth, MAX_HUE)
+    highFilter = np.array([highHue, high, high])
+    
+    return cv2.inRange(hsv, lowFilter, highFilter)
 
 def messageHandler(message):
     sys.stdout.write(".")
     #print message.topic
-    #print message.payload\
+    #print message.payload
 
 def createCamera():
     camera = cv2.VideoCapture(0)
@@ -51,45 +48,18 @@ def createCamera():
     #camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
     return camera
 
-def processRawImage(raw, params): 
+##
+## This should take a raw BGR image and determine if it contains the target we are looking for
+##
+def findTarget(raw, params): 
     mask = filterHue(raw, params["hue"], params["hueWidth"], params["low"], params["high"])
-
-    #colorOnly = cv2.bitwise_and(raw, raw, mask = mask)
-    #cv2.imshow("colormasked", colorOnly)
-    #mask = cv2.threshold(mask, params["gray"], 255, 0)
-
-    largestContour = findLargestContour(mask)
-    if largestContour is not None:
-        result = raw.copy()
-##            M = cv2.moments(largestContour)
-##            if M["m00"] != 0:
-##                cx = int(M["m10"]/M["m00"])
-##                cy = int(M["m01"]/M["m00"])
-##                cv2.circle(result, (cx, cy), 8, (250, 250, 250), -1)
-##                hull = cv2.convexHull(largestContour)
-##                cv2.drawContours(result, [hull], 0, (0,255,0), 3)
-        x,y,w,h = cv2.boundingRect(largestContour)
-        center = (x+(w/2), y+(h/2))
-        cv2.rectangle(result, (x,y), (x+w,y+h), (40,0,120), 2)
-        cv2.circle(result, center, 8, (250, 250, 250), -1)
-        cv2.putText(result, str(center[0]-320), (x-50, y+15), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,0,0), 1)
-        cv2.putText(result, str(center[1]-240), (x-50, y+45), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,0,0), 1)
-
-        #perceivedFocalLength = distanceCalc.CalculatePerceivedFocalLengthAtGivenDistance(w, targetSize[1]);
-        #params["FOV"] = int(perceivedFocalLength)
-        perceivedFocalLength = 652  
-        calc = TriangleSimilarityDistanceCalculator(targetSize[0], perceivedFocalLength)
-        distance = calc.CalcualteDistance(w);
-        cv2.putText(result, str(distance) + " inches", (5, 23), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,0,0), 1)
-
-##            tPx = w
-##            distance = params["FOV"]/w
-##            cv2.putText(result, str(distance), (30, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,0), 1)
-        #client.publish("5495.targetting", center[0]-320)
-
-        payload = { 'horizDelta': center[0] - 320, 'targetDistance': int(distance) }
-        #client.publish(MQTT_TOPIC_TARGETTING, json.dumps(payload))
-        return result
+    #cv2.imshow("mask", mask)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours) > 0:
+        ordered = sorted(contours, key = cv2.contourArea, reverse = True)[:1]
+        largestContour = ordered[0]
+        if largestContour != None and cv2.contourArea(largestContour) > params["countourSize"]:
+            return largestContour
 
 def main():
     MQTT_TOPIC_TARGETTING = "5495.targetting"
@@ -99,27 +69,30 @@ def main():
     client = mqttClient.MqttClient(host, port, topics, messageHandler)
 
     params = CVParameterGroup("Sliders", debugMode)
-    # HUES: GREEEN=65/75 BLUE=105
+    # HUES: GREEEN=65/75 BLUE=110
     params.addParameter("hue", 65, 179)
     params.addParameter("hueWidth", 5, 25)
     params.addParameter("low", 70, 255)
     params.addParameter("high", 255, 255)
-    if tuneFOV:
-        params.addParameter("FOV", 0, 2000)
-        
+    params.addParameter("countourSize", 50, 200)
+
     camera = createCamera()
-
-    distanceCalc = TriangleSimilarityDistanceCalculator(targetSize[0])
-
+    distanceCalculatorH = distanceCalculatorV  = None
+    if tuneDistance:
+        distanceCalculatorH = DistanceCalculator.TriangleSimilarityDistanceCalculator(TARGET_WIDTH)
+        distanceCalculatorV = DistanceCalculator.TriangleSimilarityDistanceCalculator(TARGET_HEIGHT)
+    else:
+        distanceCalculatorH = DistanceCalculator.TriangleSimilarityDistanceCalculator(TARGET_WIDTH, DistanceCalculator.PFL_H_LC3000)
+        distanceCalculatorV = DistanceCalculator.TriangleSimilarityDistanceCalculator(TARGET_HEIGHT, DistanceCalculator.PFL_V_LC3000)
+    
     fpsDisplay = False;
     fpsCounter = WeightedFramerateCounter()
-    fpsInterval = RealtimeInterval(3.0)
+    fpsInterval = RealtimeInterval(5.0)
 
     while (True):
         if not client.isConnected() :
             try:
-                #client.connect()
-                None
+                None#client.connect()
             except:
                 None
         
@@ -129,10 +102,47 @@ def main():
 
             if debugMode:
                 cv2.imshow("raw", raw);
-            result = processRawImage(raw, params)
-            if result != None and debugMode:
-                cv2.imshow("result", result)
-        
+
+            contour = findTarget(raw, params)
+            if target == None or not target.any():
+                payload = { 'hasTarget': False, "fps": fpsCounter.getFramerate() }
+                client.publish(MQTT_TOPIC_TARGETTING, json.dumps(payload))
+            else:
+                x,y,w,h = cv2.boundingRect(target)
+                center = (x + (w / 2), y + (h / 2))
+                horizontalOffset = center[0] - 320
+
+                distance = None
+                perceivedFocalLengthH = perceivedFocalLengthV = 0.0
+                if tuneDistance:
+                    perceivedFocalLengthH = distanceCalculatorH.CalculatePerceivedFocalLengthAtGivenDistance(w, TARGET_CALIBRATION_DISTANCE);
+                    perceivedFocalLengthV = distanceCalculatorV.CalculatePerceivedFocalLengthAtGivenDistance(h, TARGET_CALIBRATION_DISTANCE);
+                    distance = TARGET_CALIBRATION_DISTANCE
+                else:
+                    # Use the largest axis to determine the physical distance
+                    if w > h:
+                        distance = distanceCalculatorH.CalcualteDistance(w);
+                    else:
+                        distance = distanceCalculatorV.CalcualteDistance(h);
+                distance = round(distance, 1)
+
+                payload = { 'horizDelta': horizontalOffset, 'targetDistance': int(distance), 'hasTarget': True, "fps": fpsCounter.getFramerate() }
+                client.publish(MQTT_TOPIC_TARGETTING, json.dumps(payload))
+
+                if debugMode:
+                    result = raw.copy()
+                    cv2.rectangle(result, (x, y), (x + w, y + h), (40, 0, 120), 2)
+                    cv2.circle(result, center, 8, (250, 250, 250), -1)
+                    cv2.putText(result, str(horizontalOffset), (x-50, y+15), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 0), 1)
+                    if tuneDistance:
+                        cv2.putText(result, "PFL_H: {:.0f}".format(perceivedFocalLengthH), (3, 13 + 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1)
+                        cv2.putText(result, "PFL_V: {:.0f}".format(perceivedFocalLengthV), (3, 13 + 5 + 22), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1)
+                    else:
+                        cv2.putText(result, "{} inches".format(distance), (3, 13 + 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1)
+                    if fpsDisplay:
+                        cv2.putText(result, "{:.0f} fps".format(fpsCounter.getFramerate()), (640 - 100, 13 + 6), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1)
+                    cv2.imshow("result", result)
+
         if fpsDisplay and fpsInterval.hasElapsed():
             print "{0:.1f} fps".format(fpsCounter.getFramerate())
         
