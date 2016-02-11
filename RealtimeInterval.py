3e6700e4bd7107c37cb706086ae93adec7164083
John Rush <john@blakelystreet.com>
2016-02-11 02:47:59 -0800

    Improve real time throttle so that it can start immediate the first time.

--- RealtimeInterval.py
+++ RealtimeInterval.py
@@ -1,22 +1,19 @@
 import time
 
 class RealtimeInterval:
-    startTime = 0
-    interval = 0
-
-    def __init__(self, intervalInSeconds):
-        self.startTime = time.time()
+    def __init__(self, intervalInSeconds, allowImmediate = True):
         self.interval = intervalInSeconds
+        self.allowImmediate = allowImmediate
+        self.reset()
 
-    def start(self):
-        self.startTime = time.time()
+    def reset(self):
+        if self.allowImmediate:
+            self.startTime = 0
+        else:
+            self.startTime = time.time()
         
     def hasElapsed(self):
         timeNow = time.time()
-        if self.startTime == 0:
-            self.startTime = timeNow
-            return False
-    
         elapsed = timeNow - self.startTime
         if (elapsed >= self.interval):
             self.startTime = timeNow;
