585cc1a95167a13644977e599aa2f3fc9f9b7404
John Rush <john@blakelystreet.com>
2016-02-10 21:50:01 -0800

    Better handle the case where we are not showing the settings UI.

--- CVParameterGroup.py
+++ CVParameterGroup.py
@@ -2,22 +2,30 @@ import cv2
 
 class CVParameterGroup:
     def __init__(self, windowName, show = False):
+        self.values = {}
         self.show = show
         self.paramCount = 0
         self.windowName = windowName
-        cv2.namedWindow(self.windowName)
-        self.__resizeWindow()
+        if self.show:
+            cv2.namedWindow(self.windowName)
+            self.__resizeWindow()
         
     def addParameter(self, name, defaultValue, maxValue):
+        self.values[name] = defaultValue
         if self.show:
             self.paramCount += 1
             cv2.createTrackbar(name, self.windowName, defaultValue, maxValue, lambda x: None)
             self.__resizeWindow()
 
     def __getitem__(self, name):
-        return cv2.getTrackbarPos(name, self.windowName)
+        if self.show:
+            return cv2.getTrackbarPos(name, self.windowName)
+        if name in self.values:
+            return self.values[name]
+        return -1
     
     def __setitem__(self, name, value):
+        self.values[name] = value
         cv2.setTrackbarPos(name, self.windowName, value)
 
     def __resizeWindow(self):
